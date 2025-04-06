83da58564e5d4a0b3cd7fb959f3a8fd94653f5cf
katyadess <trynovakatya345@gmail.com>
2025-04-06 19:52:23 +0300

    create class Paralelogram with all attributes

--- lab_work3_task1/Program.cs
+++ lab_work3_task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Parallelogram
+{
+    public double SideA;
+    public double SideB;
+    public double Angle;
+
+    public Parallelogram(double sideA, double sideB, double angle)
+    {
+        SideA = sideA;
+        SideB = sideB;
+        Angle = angle;
+    }
+}
